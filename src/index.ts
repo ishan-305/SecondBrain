3f433d4006b86f94260839dc0e76f22f2c10436e
Ishan <ishansingh2390@gmail.com>
2025-02-21 01:25:55 +0530

    cookie-updated

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { authrouter } from "./routes/authRoutes";
 import { userrouter } from "./routes/userRoutes";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use("/api/v1/auth", authrouter);
 app.use("/api/v1/user", userrouter);
